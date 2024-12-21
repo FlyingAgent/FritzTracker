4c1d618e17cbed72d8ac1eeca05035b05504d263
FlyingAgent <joostgroen08@gmail.com>
2024-12-21 21:53:25 +0100

    Finished setup script and requirements file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-echo "Test"
+# Starting message
+echo "Starting setup process..."
+
+# VENV for the requirements to work
+python3 -m venv venv
+
+# Requirements for the project
+venv/bin/pip install -r requirements.txt
+
+# Finish message
+echo "Setup process completed!"
